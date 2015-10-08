3fe4f01359fc1f4f548e0d3885aed5862a8041d9
gaming30 <arpit.nandwani@gmail.com>
2015-10-09 02:26:34 +0530

    Module 3
    Adding interactive controls to UI

--- RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
@@ -13,28 +13,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace RestaurantManager.UniversalWindows
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class ExpeditePage : Page
     {
-        public MainPage()
+        public ExpeditePage()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Backtohome(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(OrderPage),null);
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(ExpeditePage),null);
+            this.Frame.GoBack();
         }
     }
 }
